223d6b8d79bb091f1a632e547daa51e3d344ecc8
Gaurav <gauravdeori21@gmail.com>
2026-06-13 16:55:28 +0530

    Fix undefined property crash on Firestore writes by setting ignoreUndefinedProperties to true

--- src/integrations/firebase/firebase.ts
+++ src/integrations/firebase/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
@@ -22,7 +22,9 @@ isSupported().then((supported) => {
 });
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  ignoreUndefinedProperties: true,
+});
 export const storage = getStorage(app);
 export default app;
 
